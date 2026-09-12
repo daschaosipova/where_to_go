b5fe265c53f90060097ca39fe87d464a1b23ca04
daschaosipova <daschaosipova@yandex.com>
2026-09-12 11:03:00 +0300

    Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
--- places/models.py
+++ places/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name="Название")
+    description_short = models.TextField(verbose_name="Короткое описание", blank=True)
+    description_long = models.TextField(verbose_name="Длинное описание", blank=True)
+    
+    lat = models.FloatField(verbose_name="Широта")
+    lng = models.FloatField(verbose_name="Долгота")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Интересное место"
+        verbose_name_plural = "Интересные места"
