7b4bacf8066d95545d22912d062975b56d3a76ea
daschaosipova <daschaosipova@yandex.com>
2026-09-17 22:42:24 +0300

    Embed place images into place admin page via inline

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from .models import Place, PlaceImage
+
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 0
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ("title",)
     search_fields = ("title",)
-
-admin.site.register(PlaceImage)
+    inlines = [PlaceImageInline]
